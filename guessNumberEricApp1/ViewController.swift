d114a504d802c180e4b927a52df866664728f1f1
ericwugo <ericwugo@gmail.com>
2020-09-01 13:53:05 +0800

    Initial Commit

---
+++ guessNumberEricApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessNumberEricApp1
+//
+//  Created by user on 2020/9/1.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
